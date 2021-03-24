2995252d3ca5efd9d3a1b343211c1a177547882e
Jefferson Whitney <81323940+jeffersonwhitney@users.noreply.github.com>
2021-03-24 18:00:08 -0300

    Update fibonacci_jeff.asm

--- fibonacci_jeff.asm
+++ fibonacci_jeff.asm
@@ -2,72 +2,72 @@ name "serie de fibonacci"
 
 org 100h
 
-mov bh,0         ;usei esses registradores pq nao mudaram quando fui printando no cmd
+mov bh,0         
 mov bl,1 
 
-mov dl,bh        ;pra mostrar o primeiro valor da serie
-mov ah,2         ; a funcao pra printar na tela
+mov dl,bh        
+mov ah,2         
 int 21h
-                 ;printo uma nova linha
+                 
 mov dl,0Ah
 mov ah,2
 int 21h
 
-mov dl,0Dh       ;printo pra voltar pra o inicio da linha
+mov dl,0Dh       
 mov ah,2
 int 21h 
 
-mov dl,bl        ;pra mostrar o segundo valor da serie
-mov ah,2         ; a funcao pra printar na tela
+mov dl,bl       
+mov ah,2         
 int 21h
-                 ;printo uma nova linha
+                
 mov dl,0Ah
 mov ah,2
 int 21h
 
-mov dl,0Dh       ;printo pra voltar pra o inicio da linha
+mov dl,0Dh       
 mov ah,2
 int 21h
 
 soma:
 
-add bh,bl        ;somo o valor dos dois e boto no bh
-cmp bh,233       ;como nao consegui os 16 bits comparo com o valor maximo de 4 bits
+add bh,bl        
+cmp bh,233       
 jz fim 
 
-mov dl,bh        ;printo bh
-mov cl,bh        ;tive que mover para cl pois o push so funciona pro registrador inteiro
-push cx          ;mando bh pra memoria
+mov dl,bh        
+mov cl,bh        
+push cx          
 mov ah,2
 int 21h
 
-mov dl,0Ah       ;nova linha
+mov dl,0Ah       
 mov ah,2
 int 21h
 
-mov dl,0Dh       ;inicio da linha
+mov dl,0Dh       
 mov ah,2
 int 21h
 
-add bl,bh        ;somo e armazeno no bl
-cmp bl,233       ;como nao consegui os 16 bits comparo com o valor maximo de 4 bits
+add bl,bh        
+cmp bl,233       
 jz fim
 
-mov dl,bl        ;printo bl
-mov cl,bl        ;tive que mover para cl pois o push so funciona pro registrador inteiro
-push cx          ;mando bl pra memoria
+mov dl,bl        
+mov cl,bl        
+push cx          
 mov ah,2
 int 21h
 
-mov dl,0Ah       ;nova linha
+mov dl,0Ah       
 mov ah,2
 int 21h
 
-mov dl,0Dh       ;inicio da linha
+mov dl,0Dh       
 mov ah,2
 int 21h
 
-jmp soma         ;repito tudo desde soma
+jmp soma         
 
 fim:
 ret
